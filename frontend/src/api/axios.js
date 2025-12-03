684e2de1b7802bb73d8bddf7810bb06db449fbd2
Jagapathi55 <sai.jagapathi2003@gmail.com>
2025-12-03 18:52:46 +0530

    Axios changed

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://ats-resume-ou6i.onrender.com",
   headers: { "Content-Type": "application/json" },
 });
 
